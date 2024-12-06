17bba2288173ff40b67f4145d8d6814a361f1db3
Paola B <spaolabetancourth@gmail.com>
2024-12-05 20:55:43 -0500

    Iniciando..

---
+++ src/main/java/com/alura/ChallegeLiteralura/ChallegeLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.alura.ChallegeLiteralura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallegeLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallegeLiteraluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/ChallegeLiteralura/ChallegeLiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.ChallegeLiteralura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallegeLiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
